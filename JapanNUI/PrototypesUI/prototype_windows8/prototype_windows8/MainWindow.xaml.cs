9d4a5957b94a0de0b6adac9b69bebd9b23178fdc
unknown <Kevin@Kevin-PC.(none)>
2011-11-08 16:48:01 +0900

    Prototype web browser

--- JapanNUI/PrototypesUI/prototype_windows8/prototype_windows8/MainWindow.xaml.cs
+++ JapanNUI/PrototypesUI/prototype_windows8/prototype_windows8/MainWindow.xaml.cs
@@ -17,11 +17,63 @@ namespace prototype_windows8
 	/// </summary>
 	public partial class MainWindow : Window
 	{
+		private string homepage;
 		public MainWindow()
 		{
 			this.InitializeComponent();
-
+			homepage = website.Text;
+			browser.Source = new Uri(website.Text);
 			// Insert code required on object creation below this point.
+		}
+		
+		private void Browser_loaded(object sender, System.Windows.Navigation.NavigationEventArgs e)
+		{
+			browser.Focus();
+			website.Text = browser.Source.AbsoluteUri;
+			mshtml.HTMLDocument doc = (mshtml.HTMLDocument) browser.Document;
+			onglet.Content = doc.title;
+			//MessageBox.Show(doc.title);
+		}
+		
+		private void Back_click(object sender, System.Windows.RoutedEventArgs e)
+		{
+			try
+			{
+				browser.GoBack();
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show("Pas de page précédente", "Erreur");	
+			}
+		}
+
+		private void Refresh_click(object sender, System.Windows.RoutedEventArgs e)
+		{
+			browser.Refresh();
+		}
+
+		private void Home_click(object sender, System.Windows.RoutedEventArgs e)
+		{
+			browser.Source = new Uri(homepage);
+		}
+
+		private void Forward_click(object sender, System.Windows.RoutedEventArgs e)
+		{
+			try
+			{
+				browser.GoForward();
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show("Pas de page suivante", "Erreur");	
+			}
+		}
+
+		private void Go_website(object sender, System.Windows.Input.KeyEventArgs e)
+		{
+			if (e.Key.Equals(Key.Return)){
+				browser.Source = new Uri(website.Text);
+			}
 		}
 	}
 }
