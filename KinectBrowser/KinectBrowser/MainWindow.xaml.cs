9fc9d0dddf5c374d1251eabcc32a93f9ca962fc7
Lukas <karim.luccin@gmail.com>
2011-11-22 15:08:23 +0900

    KinectBrowser first

---
+++ KinectBrowser/KinectBrowser/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using KinectBrowser.D3D;
+
+namespace KinectBrowser
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public SoraEngineHost SoraEngine { get; private set; }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            SoraEngine = new SoraEngineHost((int)ActualWidth, (int)ActualHeight);
+            SoraEngine.Initialize();
+
+            CompositionTarget.Rendering += new EventHandler(CompositionTarget_Rendering);
+        }
+
+        void CompositionTarget_Rendering(object sender, EventArgs e)
+        {
+            SoraEngine.Render();
+        }
+    }
+}
---
+++ KinectBrowser/Runtime/CopyRuntimesToBin.bat
@@ -0,0 +1,2 @@
+copy *.dll ..\bin\*
+copy *.pdb ..\bin\*
