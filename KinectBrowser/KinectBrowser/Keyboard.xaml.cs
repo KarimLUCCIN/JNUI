a1dffe88fb46d7ec9a2cd754daac21a14cf6b5f2
unknown <Kevin@Kevin-PC.(none)>
2011-12-22 15:34:21 +0900

    Debut design clavier virtuel

---
+++ KinectBrowser/KinectBrowser/Keyboard.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace KinectBrowser
+{
+	/// <summary>
+	/// Interaction logic for Keyboard.xaml
+	/// </summary>
+	public partial class Keyboard : UserControl
+	{
+		public Keyboard()
+		{
+			this.InitializeComponent();
+		}
+	}
+}
